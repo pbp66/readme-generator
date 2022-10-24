a27c5fb5076b6083c403e9ee33869edf3bc811b4
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-23 22:57:35 -0400

    adjusted formatting in js files

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // TODO: Include packages needed for this application
+// Is this right?
+import defaultExport from "generateMarkdown";
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
