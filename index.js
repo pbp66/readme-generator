e52123cc70a7662193b8fe1dff47d22dc70b95d4
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-23 23:21:15 -0400

    Added first set of questions to const questions array

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // TODO: Include packages needed for this application
 // Is this right?
-import defaultExport from "generateMarkdown";
+//import defaultExport from "generateMarkdown";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of the project?", "Update the project description.", "Update the installation instructions.", "Update the usage information.", "Update the contribution guidelines.", "Update the test instructions", "Update the project license.", "Update the project owner GitHub user name.", "Update project owner email address."];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -12,7 +12,11 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-
+    console.log("\n");
+    for (let i = 0; i < questions.length; i++) {
+        console.log(questions[i]);
+    }
+    console.log("\n");
 }
 
 // Function call to initialize app
