eb06b2efc4767ee3df4dae3ea02c1ca13a6934f7
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-23 23:34:04 -0400

    Added empty function askQuestions()

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 // Is this right?
 //import defaultExport from "generateMarkdown";
-import inquirer from 'inquirer';
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = ["What is the title of the project?", "Update the project description.", "Update the installation instructions.", "Update the usage information.", "Update the contribution guidelines.", "Update the test instructions", "Update the project license.", "Update the project owner GitHub user name.", "Update project owner email address."];
@@ -13,11 +13,15 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log("\n");
-    for (let i = 0; i < questions.length; i++) {
-        console.log(questions[i]);
-    }
-    console.log("\n");
+
+}
+
+function askQuestions() {
+    // let answers = [];
+    // inquirer.prompt(questions, answers)
+    //     .then((answers) => {
+    //         console.log(answers);
+    //     });
 }
 
 // Function call to initialize app
