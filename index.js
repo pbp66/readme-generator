dbcfdaeecbab3a36b107297dcf88cadf023f5211
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-24 20:22:09 -0400

    Added file server module to script

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ class Question {
 // TODO: Include packages needed for this application
 // Is this right?
 //import defaultExport from "generateMarkdown";
-const inquirer = require('inquirer');
+const inquirer = require('inquirer'); // Load inquirer module for clean user input
+const fs = require('fs'); // Load file server module for file I/O
 
 // TODO: Create an array of questions for user input
 const questions = ["What is the title of the project?", "Update the project description.", "Update the installation instructions.", "Update the usage information.", "Update the contribution guidelines.", "Update the test instructions", "Update the project license.", "Update the project owner GitHub user name.", "Update project owner email address."];
