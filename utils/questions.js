92efb3e66864d5567009f54e60d6570866065db0
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-24 23:06:16 -0400

    Moved Question Class to questions.js

--- utils/questions.js
+++ utils/questions.js
@@ -3,5 +3,22 @@ class SimpleQuestion {
 }
 
 class Question extends SimpleQuestion {
-    
+    constructor(answerType, questionName, question) {
+        this.type = answerType; //editor launches a text editor. For basic input, use input
+        this.name = questionName;
+        this.message = question;
+        this.default = ""; // If no answer is provided by the user
+
+        this.choices;
+        this.validate;
+        this.filter;
+        this.transformer;
+        this.when;
+        this.pageSize;
+        this.prefix;
+        this.suffix;
+        this.askAnswered;
+        this.loop;
+        this.waitUserInput = true;
+    }
 }
