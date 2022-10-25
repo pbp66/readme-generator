3dd1cbfa8665e9b443377402a3b4a04c3e04a503
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-24 23:32:54 -0400

    Implemented basic Question class constructor and methods

--- utils/questions.js
+++ utils/questions.js
@@ -6,22 +6,105 @@ class SimpleQuestion {
 }
 
 class Question extends SimpleQuestion {
-    constructor(answerType, questionName, question) {
-        this.type = answerType; //editor launches a text editor. For basic input, use input
-        this.name = questionName;
-        this.message = question;
-        this.default = ""; // If no answer is provided by the user
+    // Private Class Attributes
+    #validate;
+    #filter;
+    #transformer;
+    #when;
+    #pageSize;
+    #prefix;
+    #suffix;
+    #askAnswered;
+    #loop;
+    
+    // Constructor
+    constructor(question, title, answerType="input", defaultAnswer = "", choices = []) {
+        super(question, title); 
+        this.type = answerType; //editor launches a text editor. For basic input, use input       
+        this.default = defaultAnswer; // If no answer is provided by the user, use default
 
-        this.choices;
-        this.validate;
-        this.filter;
-        this.transformer;
-        this.when;
-        this.pageSize;
-        this.prefix;
-        this.suffix;
-        this.askAnswered;
-        this.loop;
-        this.waitUserInput = true;
+        this.message = this.question; // Provided by calling super
+        this.name = this.title; // Provided by calling super
+
+        if (choices != undefined && choices.length > 0) {
+            this.choices = choices;
+            this.#pageSize = choices.length;
+        }
+
+        if (answerType === "editor") {
+            this.waitUserInput = true;
+        }
+    }
+
+    getValidator() {
+        return this.#validate;
+    }
+    
+    setValidator(validator) {
+        this.#validate = validator;
+    }
+    
+    getFilter() {
+        return this.#filter;
+    }
+    
+    setFilter(filter) {
+        this.#filter = filter;
+    }
+    
+    getTransformer() {
+        return this.#transformer;
+    }
+    
+    setTransformer(transformer) {
+        this.#transformer = transformer;
+    }
+    
+    getWhen() {
+        return this.#when;
+    }
+    
+    setWhen(when) {
+        this.#when = when;
+    }
+    
+    getPageSize() {
+        return this.#pageSize;
+    }
+    
+    setPageSize(pageSize) {
+        this.#pageSize = pageSize;
+    }
+    
+    getPrefix() {
+        return this.#prefix;
+    }
+    
+    setPrefix(prefix) {
+        this.#prefix = prefix;
+    }
+    
+    getSuffix() {
+        return this.#suffix;
+    }
+    
+    setSuffix(suffix) {
+        this.#suffix = suffix;
+    }
+    
+    getAskAnswered() {
+        return this.#askAnswered;
+    }
+    
+    setAskAnswered(askAnswered) {
+        this.#askAnswered = askAnswered;
+    }
+    
+    getLoop() {
+        return this.#loop;
+    }
+
+    setLoop(loop) {
+        this.#loop = loop;
     }
 }
