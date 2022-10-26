6629804ac00e389fd372e6957064cd8ba7ae644a
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-25 23:16:18 -0400

    Created main function in index.js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const fs = require('fs'); // Load file server module for file I/O
 const markdown = require('./utils/markdown.js'); // Load markdown.js class
 const question = require('./utils/questions.js'); // Load questions.js class definitions
 const answer = require('./utils/answer.js'); // Load answer.js class
+const { mainModule } = require('process');
 
 // TODO: Create an array of questions for user input
 const questions = ["What is the title of the project?", "Update the project description.", "Update the installation instructions.", "Update the usage information.", "Update the contribution guidelines.", "Update the test instructions", "Update the project license.", "Update the project owner GitHub user name.", "Update project owner email address."];
 
+// TODO: Change to the keys of an answer class instance
 const questionTitles = ["title", "description", "installation", "usage", "contribution", "testing", "license", "username", "email"];
 
 // TODO: Create a function to write README file
@@ -30,13 +32,15 @@ function init() {
     return new question.Questions(questions, questionTitles);
 }
 
-// Function call to initialize app
-let questionObjs = init();
-//console.log(questionObjs);
+function main() {
+    // Function call to initialize app
+    let questionObjs = init();
+    //console.log(questionObjs);
 
-askQuestions(questionObjs.questions);
+    askQuestions(questionObjs.questions);
+}
 
-
+main();
 
 // DEV TESTING SECTION
 
--- utils/answer.js
+++ utils/answer.js
@@ -10,6 +10,4 @@ class Answer {
         this.username;
         this.email;
     }
-
-
 }
