7965621947bf0883b80781538ea6efc1b2533274
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-25 23:09:32 -0400

    Created answer.js and Answer class

---
+++ utils/answer.js
@@ -0,0 +1,15 @@
+class Answer {
+    constructor() {
+        this.title;
+        this.description;
+        this.installation;
+        this.usage;
+        this.contribution;
+        this.testing;
+        this.license;
+        this.username;
+        this.email;
+    }
+
+
+}
