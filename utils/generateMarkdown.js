3908f271819e314e9e401e997d3cac892c254c5b
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-25 22:51:30 -0400

    Created contstructor for Markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,38 +1,53 @@
 class Markdown {
     constructor(dataToRender) {
+        this.title = this.generateTitleSection(dataToRender.title);
+        this.description = this.generateDescriptionSection(dataToRender.description);
+        this.installation = this.generateInstallationSection(dataToRender.installation);
+        this.usage = this.generateUsageSection(dataToRender.usage);
+        this.contribution = this.generateContributionSection(dataToRender.contribution);
+        this.testing = this.generateTestingSection(dataToRender.testing);
+        this.license = this.generateLicenseSection(dataToRender.license);
+        this.questions = this.generateQuestionsSection(dataToRender.username, dataToRender.email);
+        //this.credits = this.generateCreditsSection(dataToRender);
+        this.toc = this.generateTOC();
 
+        this.generateMarkdown();
     }
 
-    generateTitleSection() {
+    generateTitleSection(title) {
         
     }
 
-    generateDescriptionSection() {
+    generateDescriptionSection(description) {
         
     }
 
-    generateInstallationSection() {
+    generateInstallationSection(installation) {
         
     }
 
-    generateUsageSection() {
+    generateUsageSection(usage) {
         
     }
 
-    generateContributionSection() {
+    generateContributionSection(contribution) {
         
     }
 
-    generateTestingSection() {
+    generateTestingSection(testing) {
         
     }
 
-    generateQuestionsSection() {
+    generateQuestionsSection(questions) {
         
     }
 
     generateTOC() {
         
+    }
+
+    generateCreditsSection() {
+
     }
     
     generateLicense(license) {
@@ -41,73 +56,49 @@ class Markdown {
         this.#renderLicenseSection();
     }
 
-    generateMarkdown(data) {
-
-        `# ${data.title}
+    generateMarkdown() {
+        let markdown = 
+        `
+        # ${this.title}
 
         ## Description
         
-        ${data.description}
+        ${this.description}
         
         ## Table of Contents
         
-        - [Installation](#installation)
-        - [Usage](#usage)
-        - [Contribution](#contribution)
-        - [Testing](#testing)
-        - [Questions](#questions)
-        - [Credits](#credits)
-        - [License](#license)
+        ${this.toc}
         
         ## Installation
         
-        ${data.installation}
+        ${this.installation}
         
         ## Usage
         
-        Provide instructions and examples for use. Include screenshots as needed.
-        
-        To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-        
-            ```md
-            ![alt text](assets/images/screenshot.png)
-            ```
-        
+        ${this.usage}
+
         ## Credits
         
-        List your collaborators, if any, with links to their GitHub profiles.
-        
-        If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-        
-        If you followed tutorials, include links to those here as well.
+        ${this.credits}
         
         ## License
         
-        The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-        
-        ---
-        
-        🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-        
-        ## Badges
-        
-        ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-        
-        Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+        ${this.license}
         
         ## Features
         
         If your project has a lot of features, list them here.
         
         ## How to Contribute
-        
-        If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+        ${this.contribution}
         
         ## Tests
         
-        Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+        ${this.testing}
+        `
 
-        return `# ${data.title}`;
+        return markdown;
     }
 
     // If there is no license, return an empty string
