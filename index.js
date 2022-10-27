b4762dbf17deae2af51056f50bb12defb3069df4
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-26 23:01:30 -0400

    Added dynamic filePath feature

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require('fs'); // Load file system module for file I/O
 const question = require('./utils/questions.js'); // Load questions.js class definitions
 const a = require('./utils/answer.js'); // Load answer.js class
 
-function writeToFile(fileName, data) {
-    fs.writeFile(`./${fileName}`, data, err => {
+function writeToFile(filePath, data) {
+    fs.writeFile(`${filePath}`, data, err => {
         if (err) {
             console.error(err);
         }
@@ -12,7 +12,7 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-    // TODO: Update array of questions for user input. Use template questions instead?
+    let titles = Object.keys(new a.Answer());
     const questions = [
         "Provide the project title: ", 
         "Provide the project description: ", 
@@ -24,15 +24,19 @@ function init() {
         "Provide the project owner's GitHub user name: ", 
         "Provide the project owner's email address: "
     ];
-    let titles = Object.keys(new a.Answer());
     return new question.Questions(questions, titles);
 }
 
 async function main() {
     // Function call to initialize app
+    let filePath = "./README.md";
     let questionObjs = init();
+    if (process.argv[2] != undefined) {
+        filePath = process.argv[2];
+    }
+
     let markdownContent = await questionObjs.askQuestions();
-    writeToFile("README.md", markdownContent);
+    writeToFile(filePath, markdownContent);
 }
 
 main();
